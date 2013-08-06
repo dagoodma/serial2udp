72b428591428b34a724d23159af98fd9bb861737
David Goodman <dagoodma@gmail.com>
2013-08-05 17:38:43 -0700

    Added modified serial2udp source file. Updated README.md

--- README.md
+++ README.md
@@ -13,13 +13,21 @@ This program is GUI-less and runs directly from the command line. Details on ava
 NOTE: You should specify a packet size for both the serial and UDP lines to get optimal performance. This will specify how many bytes the program waits for before triggering an interrupt on the serial side. On the UDP
 side it must be larger than the largest expected UDP datagram size otherwise the program may not work properly.
 
+### SLUGS Parser
+
+This version of serial2udp has been modified for use as a communication bridge for the SLUGS autopilot while in hardware in the loop (HIL) simulation. This invovles converting UDP packets from the HIL simulation model in Simulink to MAVLink messages to be sent to the autopilot. The parser will also parse MAVLink messages from the autopilot and pack them into UDP packets to be sent to Simulink. 
+
+To accomplish this, many arguments were added to serial2udp to allow for separate UDP receive and send sockets. This was tested and works on Windows 7 with a Visual Studio 2010 project. Additional folders were added to the include path to make this work. To obtain the Visual 2010 project as is, see the 'slugs_parser_vs10' branch.
+
 ## Building
 
-### Windows 64-bit
+### Windows 64-bit or 32-bit
 
 Prerequisites:
  - Compiler: Only tested with Microsoft Visual Studio 2010
  - Boost library 
+ - MAVLink library with SLUGS messages
+ - inttypes.h for MAVLink
 
 #### Install Boost (http://www.boost.org/)
 1. Download the Boost library from boost.org
@@ -27,38 +35,43 @@ Prerequisites:
 3. (Following the build instructions for x86-64 1.51) Open a command prompt and enter the root directory of the Boost files.
 4. Run `bootstrap.bat`
 5. Compile for x86-64 by running `.\b2 --toolset=msvc-10.0 variant=release link=static threading=multi runtime-link=static architecture=x86 address-mode=64`
+  - Remove 'address-mode=64' if compiling for x86.
   - This will build only the multi-threaded statically-linked release build that is necessary for this project. If you'd like to use this Boost installation for debugging or other projects you'll probably want to just build everything by switching out the 'variant=release link=static threading=multi runtime-link=static' options for '--build-type=complete' instead.
 
 #### Building the program
 
-I have only tested this program on 64-bit Windows and as such have no instructions for compiling on other platforms. This program only relies on standard library functions as well as the cross-platform C++ library Boost and as such should be retargetable to many platforms.
+I have only tested this program on Windows 7 and as such have no instructions for compiling on other platforms. This program only relies on standard library functions as well as the cross-platform C++ library Boost and as such should be retargetable to many platforms.
 
-I only include instructions for building this project for x64 as that is my primary target. This code should compile for x86 if you have the x86 build of Boost available.
+I only include instructions for building this project for x64 and x86 as that is my primary target. 
 
 1. Create a new MSVC++ 2010 Win32 Console Application project
   - Specify Empty Project within the wizard
-2. Add serial2udp.cpp to the project by right-clicking on the Sources folder in the Solution Explorer.
-3. Modify the project to add the root Boost folder ('C:\boost_1_51_0' if following the above instructions) to the header path
+2. Add 'serial2udp.cpp' and 'slugs_parser\SlugsMavlink.cpp' to the project by right-clicking on the Sources folder in the Solution Explorer.
+3. Add 'slugs_parser\Packing.h' and 'slugs_parser\SlugsMavlink.h' to the project by right-clicking on the Header Files folder in the Solution Explorer.
+4. Visual Studio does not support C types, so inttypes.h must be obtained and copied into the root of your project's source folder (ie. 'MyProject/MyProject' if your VS2010 project were called 'MyProject'). This header can be found under the 'slugs_parser_vs10' branch at 'HILBridgeClient\HILBridgeClient\inttypes.h'.
+5. Modify the project to add the folder with inttypes.h ('.\' if following step 4), the root Boost folder ('C:\boost_1_51_0' if following the above instructions), and the root of the SLUGS MAVLink include folder to the header path
   - Properties->C/C++->General->Additional Include Directories
-4. Modify the project to use the static non-debug version of the Boost library 
+6. Modify the project to use the static non-debug version of the Boost library 
   - Properties->C/C++->Code Generation->Runtime Library
-5. Modify the project to add the Boost libraries for linking ('C:\boost_1_51_0\stage\lib\' if following the above instructions)
+7. Modify the project to add the Boost libraries for linking ('C:\boost_1_51_0\stage\lib\' if following the above instructions)
   - Properties->C/C++->Linker->General->Additional Library Directories
-6. Disable use of precompiled headers
+8. Disable use of precompiled headers
   - Set Properties->C/C++->Precompiled Headers to 'Not Using Precompiled Headers'
-7. Set compilation for a statically-linked multi-threaded library
+9. Set compilation for a statically-linked multi-threaded library
   - Set Properties->C/C++->Code Generation->Runtime Library to 'Multi-threaded (/MT)'
-8. Specify building for an x64 release target
+10. Specify building for an x64 release target. Skip this step if you are using x86
   - Build->Configuration Manager->Active Solution Platform->New...->OK
   - Under 'Platform'  select the new x64 target.
   - Under 'Configuration' select 'Release'
-9. Build it
+11. Build it
   - Ctrl-Shift-B or Build->Build Solution
-10. Run it
-  - The executeable will be in '/x64/Release/'
+12. Run it
+  - The executeable will be in '/x64/Release/' or under '/Release/' for x86.
 
 
 ### Mac OS X 10.6.8 (Snow Leopard) 32-bit (i386)
+
+The SLUGS parser has not been tested on Mac, and instructions to compile with MAVLink are not shown below. You will have to figure this out on your own.
 
 Prerequisites:
  - Compiler: Only tested with g++
@@ -66,7 +79,7 @@ Prerequisites:
 
 #### Install Boost (http://www.boost.org/)
 
-Installing Boost with a package manager such as MacPorts, Fink, or Homebrew should also work, but I kept seeing 'unsupported file format' warnings and undefined symbols when attempting to link the Boost libraries. Thus it is recommended to build Boost from source:
+Installing Boost with a package manager such as MacPorts, Fink, or Homebrew should also work, but I kept seeing 'unsupported file format' warnings and undefined symbols when attempting to link the Boost libraries. Thus it is recommended to build Boost from source: 
 
 1. Download the Boost library from boost.org
 2. Extract the library to a folder in your user directory (ie. ~/Downloads)
--- serial2udp.cpp
+++ serial2udp.cpp
@@ -8,6 +8,8 @@
 #define _WIN32_WINNT 0x0501
 #endif
 
+#define PARSE_SLUGS_MAVLINK
+
 #include <iostream>
 #include <string>
 #include <boost/array.hpp>
@@ -15,6 +17,10 @@
 #include <boost/asio.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/program_options.hpp>
+#ifdef PARSE_SLUGS_MAVLINK
+#include <boost/tuple/tuple.hpp>
+#include "slugsMavlinkParser.h"
+#endif
 
 // Set up some shorthand for namespaces
 namespace po = boost::program_options;
@@ -45,10 +51,20 @@ void print_error(const string message);
 int opt_baud_rate;
 string opt_port;
 int opt_remote_socket;
-string opt_remote_addr;
+string opt_remote_rx_addr;
+string opt_remote_tx_addr;
 int opt_local_socket;
-int opt_udp_packet_size;
+int opt_udp_rx_packet_size;
+int opt_udp_tx_packet_size;
 int opt_serial_packet_size;
+// SLUGS parser specific options
+#ifdef PARSE_SLUGS_MAVLINK
+int opt_autopilot_system_id;
+int opt_autopilot_comp_id;
+int opt_gs_system_id;
+int opt_gs_comp_id;
+SlugsMavlinkParser *parser;
+#endif
 
 int main(int ac, char* av[])
 {
@@ -61,11 +77,19 @@ int main(int ac, char* av[])
 			("help", "produce help message")
 			("port", po::value<string>(&opt_port)->default_value("COM1"), "serial port to use")
 			("baud_rate", po::value<int>(&opt_baud_rate)->default_value(115200), "set the serial port baud rate")
-			("serial_packet_size", po::value<int>(&opt_serial_packet_size)->default_value(22), "size of the serial packets to be expected (<= 128)")
-			("local_socket", po::value<int>(&opt_local_socket)->default_value(14550), "local UDP socket to use")
-			("remote_socket", po::value<int>(&opt_remote_socket)->default_value(16000), "remote UDP socket to use")
-			("remote_addr", po::value<string>(&opt_remote_addr)->default_value("127.0.0.1"), "remote IP address")
-			("udp_packet_size", po::value<int>(&opt_udp_packet_size)->default_value(28), "size of a UDP datagram in bytes (<= 128)")
+			("local_socket", po::value<int>(&opt_local_socket)->default_value(5679), "local UDP socket to receive on")
+			("remote_socket", po::value<int>(&opt_remote_socket)->default_value(5678), "remote UDP socket to trasmit on")
+			("remote_tx_addr", po::value<string>(&opt_remote_tx_addr)->default_value("255.255.255.255"), "remote IP address to receive from")
+			("remote_rx_addr", po::value<string>(&opt_remote_rx_addr)->default_value("any"), "remote IP address to receive from")
+			("udp_rx_packet_size", po::value<int>(&opt_udp_rx_packet_size)->default_value(113), "size of received UDP datagram in bytes (<= 128)")
+			("udp_tx_packet_size", po::value<int>(&opt_udp_tx_packet_size)->default_value(20), "size of transmitted UDP datagram in bytes (<= 128)")
+			("serial_packet_size", po::value<int>(&opt_serial_packet_size)->default_value(100), "size of the serial packets to be expected (<= 128)")
+			#ifdef PARSE_SLUGS_MAVLINK
+			("autopilot_sys_id", po::value<int>(&opt_autopilot_system_id)->default_value(100), "set autopilot system id for MAVLink (<=255)")
+			("autopilot_comp_id", po::value<int>(&opt_autopilot_comp_id)->default_value(1), "set autopilot component id for MAVLink (<=255)")
+			("gs_sys_id", po::value<int>(&opt_gs_system_id)->default_value(127), "set ground station system id for MAVLink (<=255)")
+			("gs_comp_id", po::value<int>(&opt_gs_comp_id)->default_value(0), "set ground station component id for MAVLink (<=255)")
+			#endif
 		;
 
 		// Now parse the user-specified options
@@ -79,7 +103,7 @@ int main(int ac, char* av[])
 			return 0;
 		}
 
-		udp_receive_buffer = new char[opt_udp_packet_size];
+		udp_receive_buffer = new char[opt_udp_rx_packet_size];
 		if (udp_receive_buffer == NULL) {
 			cerr << "ERROR: Could not allocate enough space for a UDP receive buffer of that size." << endl;
 			return 3;
@@ -95,16 +119,50 @@ int main(int ac, char* av[])
 		cerr << e.what() << endl;
 		return 1;
 	}
+
+	// Create a parser
+	#ifdef PARSE_SLUGS_MAVLINK
+	try {
+		// Initialize SLUGS parser
+		parser = new SlugsMavlinkParser(opt_autopilot_system_id, opt_autopilot_comp_id, opt_gs_system_id, opt_gs_comp_id);
+		if (!parser->isOk()) {
+			cerr << "ERROR: Failed to initialize the SLUGS parser." << endl;
+			return 4;
+		}
+		cout << "Initialized SLUGS MAVLink parser." << endl;
+	} catch (exception& e) {
+		cerr << "Failed to construct parser: " << e.what() << endl;
+		return 4;
+	}
+	#endif
 	
 	// Create and start the UDP socket.
 	boost::asio::io_service io_service;
 	try {
+		// Parse addresses
+		bool udp_receive_any = (opt_remote_rx_addr.compare("any") == 0)? true : false; 
+		bool udp_transmit_broadcast = (opt_remote_tx_addr.compare("255.255.255.255") == 0)? true : false;
+		boost::asio::ip::address_v4 remote_rx_addr = (udp_receive_any)?
+			boost::asio::ip::address_v4::any() : 
+			boost::asio::ip::address_v4::from_string(opt_remote_rx_addr);
+
+		boost::asio::ip::address_v4 remote_tx_addr = (udp_transmit_broadcast)?
+			boost::asio::ip::address_v4::broadcast() : 
+			boost::asio::ip::address_v4::from_string(opt_remote_tx_addr);
+
 		// Set up the endpoints. These are the local and remote server definitions (IP + socket)
-		remote_endpoint_tx = boost::asio::ip::udp::endpoint(boost::asio::ip::address_v4::from_string(opt_remote_addr), opt_remote_socket);
-		remote_endpoint_rx = boost::asio::ip::udp::endpoint(boost::asio::ip::address_v4::from_string(opt_remote_addr), 0);
+		remote_endpoint_tx = boost::asio::ip::udp::endpoint(boost::asio::ip::address_v4::broadcast(), opt_remote_socket);
+		remote_endpoint_rx = boost::asio::ip::udp::endpoint(remote_rx_addr, opt_local_socket);
 
 		// Start a Boost IO service and attach a new UDP socket
-		udpSocket = new boost::asio::ip::udp::socket(io_service, boost::asio::ip::udp::endpoint(boost::asio::ip::address_v4::loopback(), opt_local_socket));
+		udpSocket = new boost::asio::ip::udp::socket(io_service, boost::asio::ip::udp::endpoint(boost::asio::ip::address_v4::any(), opt_local_socket));
+		
+		// Allow broadcasting if desired
+		if (udp_transmit_broadcast) {
+			boost::asio::socket_base::broadcast option(true);
+			udpSocket->set_option(option);
+		}
+
 		start_udp_receive();
 
 	} catch (exception& e) {
@@ -113,8 +171,8 @@ int main(int ac, char* av[])
 	}
 	
 	cout << "Listening at localhost:" << opt_local_socket << "." << endl;
-	cout << "Listening for messages from " << opt_remote_addr << ", any port." << endl;
-	cout << "Transmitting to " << opt_remote_addr << ":" << opt_remote_socket << "." << endl; 
+	cout << "Listening for messages from " << opt_remote_rx_addr << ":" << opt_local_socket << "." << endl;
+	cout << "Transmitting to " << opt_remote_tx_addr << ":" << opt_remote_socket << "." << endl; 
 
 	// Create and start the serial port.
 	try {
@@ -153,7 +211,7 @@ int main(int ac, char* av[])
 void start_udp_receive()
 {
 	udpSocket->async_receive_from(
-		boost::asio::buffer(udp_receive_buffer, opt_udp_packet_size), 
+		boost::asio::buffer(udp_receive_buffer, opt_udp_rx_packet_size), 
 		remote_endpoint_rx,
 		&handle_udp_receive
 	);
@@ -171,20 +229,60 @@ void start_serial_receive()
 	);
 }
 
+
+
+void print_hex(const char *s)
+{
+  while(*s)
+    printf("%02x", (unsigned int) *s++);
+  printf("\n");
+}
+
 /**
  * This function is called asynchronously after a UDP datagram is received. It just packages up that
  * same data for an asynchronous transmit out the serial port. Any errors are printed to the screen.
  */
 void handle_udp_receive(const boost::system::error_code& error, size_t bytes_transferred)
 {
-	// Once a UDP datagram is received, send it out over the serial port. If the reception was unsuccessfu,	l	
+	//cout << "HERE!" << endl;
+	// Once a UDP datagram is received, send it out over the serial port. If the reception was unsuccessful,	
 	// print an error message.
+	//cout << "Got UDP " << bytes_transferred << " bytes." << endl;
 	if (!error || error == boost::asio::error::message_size) {
+
+        // Simulink to Autopilot (udp to mavlink serial)
+        #ifdef PARSE_SLUGS_MAVLINK
+		///*
+		size_t old_size = bytes_transferred;
+		boost::tuple<uint8_t*, size_t> t = parser->parse_udp2serial(
+			reinterpret_cast<uint8_t*>(udp_receive_buffer), bytes_transferred);
+		char *udp_receive_buffer = reinterpret_cast<char*>(t.get<0>());
+		size_t bytes_transferred = t.get<1>();
+		//*/
+
+		// My Tests (bad form to put it here)
+		/*
+		cout << "Raw MAVLINK: ";
+		print_hex(udp_receive_buffer);
+		// Parse mavlink
+		mavlink_message_t msg;
+		mavlink_status_t status;
+		for (uint16_t i = 0; i < bytes_transferred; i++) {
+			mavlink_parse_char(0, udp_receive_buffer[i], &msg, &status);
+		}
+		printf("Going to send MAVLink message (%d) with status: %d.\r\n", msg.msgid, status.parse_error);
+		//cout << "Going to send MAVLink message (" << msg.msgid << ") with status: " << status.parse_error << endl;
+		*/
+        #endif
+
 		boost::asio::async_write(
 			*serialPort,
-			boost::asio::buffer(udp_receive_buffer, bytes_transferred),
+            boost::asio::buffer(udp_receive_buffer, bytes_transferred),
 			&handle_serial_send
 		);
+
+		//cout << "Received and forwarded " << bytes_transferred << " UDP bytes." << endl;
+		//cout << "Received " << old_size << " UDP bytes and transmitted: " << bytes_transferred << "." << endl;
 	} else if (error != boost::asio::error::connection_refused) {
 		print_error(error.message());
 	}
@@ -204,14 +302,26 @@ void handle_udp_send(const boost::system::error_code& error, size_t bytes_transf
 	}
 }
 
+
 /**
  * This function deals with a buffer full of serial data. More specifically it sends the buffer off in a UDP
  * datagram if there isn't an error and resumes waiting.
+ * TODO add mavlink message parsing here
  */
 void handle_serial_receive(const boost::system::error_code& error, size_t bytes_transferred)
 {
 	// If there isn't an error or the error is just one about message size
 	if (!error || error == boost::asio::error::message_size) {
+    
+        // Autopilot to UDP (serial mavlink to simulink udp)
+        // TODO Need to wait for LOGSIZE=254 to send udp packet out
+        #ifdef PARSE_SLUGS_MAVLINK
+		boost::tuple<uint8_t*, size_t> t = parser->parse_serial2udp(
+			reinterpret_cast<uint8_t*>(serial_receive_buffer), bytes_transferred);
+		char *serial_receive_buffer = reinterpret_cast<char*>(t.get<0>());
+		size_t bytes_transferred = t.get<1>();
+        #endif
+
 		udpSocket->async_send_to(
 			boost::asio::buffer(serial_receive_buffer, bytes_transferred),
 			remote_endpoint_tx,
